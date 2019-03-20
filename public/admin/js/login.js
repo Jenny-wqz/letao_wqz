28f5618d605732d5a3db5cd2037b8945b2047d97
Jenny-wqz <952474780@qq.com>
2019-03-20 16:29:32 +0800

    登陆功能基本完成

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -2,6 +2,14 @@ var $form = $("#form");
 
 // 初始化校验
 $form.bootstrapValidator({
+  // 指定校验时的图标显示，默认是bootstrap风格
+  feedbackIcons: {
+    valid: "glyphicon glyphicon-ok",
+    invalid: "glyphicon glyphicon-remove",
+    validating: "glyphicon glyphicon-refresh"
+  },
+
+  // 指定校验字段
   fields: {
     //校验用户名,对应用户表单的name属性
     username: {
@@ -14,6 +22,9 @@ $form.bootstrapValidator({
           min: 3,
           max: 9,
           message: "用户名长度必须在3到9之间"
+        },
+        callback: {
+          message: "用户名不存在!"
         }
       }
     },
@@ -29,6 +40,9 @@ $form.bootstrapValidator({
           min: 6,
           max: 12,
           message: "密码长度必须在6-12之间"
+        },
+        callback: {
+          message: "密码错误!"
         }
       }
     }
@@ -42,17 +56,32 @@ $("#form").on("success.form.bv", function(e) {
   $.ajax({
     url: "/employee/employeeLogin",
     type: "post",
-    dataType: "json",
     data: $form.serialize(),
     success: function(res) {
       //   console.log(res);
       if (res.error === 1000) {
-        // console.log("用户名错误");
+        //如果失败了,提醒用户不存在
+        $form
+          .data("bootstrapValidator")
+          .updateStatus("username", "INVALID", "callback");
       } else if (res.error === 1001) {
-        // console.log("密码错误");
+        //提醒用户密码错误
+        $form
+          .data("bootstrapValidator")
+          .updateStatus("password", "INVALID", "callback");
       } else if (res.success === true) {
         // console.log("登陆成功");
+        location.href = "index.html";
       }
     }
   });
+});
+
+//点击重置按钮,重置表单
+$("[type=reset]").on("click", function() {
+  //由于reset类型的表单能够自动清空表单中的内容,所以在这里不需要再传参
+  // $form.data("bootstrapValidator").resetForm();
+  //如果只是一个单纯的按钮,需要在resetForm方法中传一个true
+  // $form.data("bootstrapValidator").resetForm();
+  $form.data("bootstrapValidator").resetForm(true);
 });
